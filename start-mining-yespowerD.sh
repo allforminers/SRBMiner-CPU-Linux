c6dfcd1f303f055ef0a583ef78724f71443f796e
allforminers <ftresfi@gmail.com>
2021-11-15 20:51:26 +0100

    Create start-mining-yespowerD.sh

---
+++ start-mining-yespowerD.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+
+./SRBMiner-MULTI --disable-gpu --algorithm yespower --pool 162.55.244.234:6234 --wallet WTgGg9Uw9B1qVrSTX7RWaHoSCP2tHbyUsK.digi --password c=WHIVE 
+pause
