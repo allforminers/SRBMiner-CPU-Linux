adf07e18f633a9d73e1cde8a8eec4def1cd230e3
allforminers <65371946+allforminers@users.noreply.github.com>
2021-11-15 20:45:13 +0100

    Update start-mining-yespower.sh

--- start-mining-yespower.sh
+++ start-mining-yespower.sh
@@ -3,5 +3,5 @@
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
 
-./SRBMiner-MULTI --disable-gpu --algorithm yespower --pool 162.55.244.234:6234 --wallet WTgGg9Uw9B1qVrSTX7RWaHoSCP2tHbyUsK.vul --password c=WHIVE 
+./SRBMiner-MULTI --disable-gpu --algorithm yespower --pool 162.55.244.234:6234 --wallet WTgGg9Uw9B1qVrSTX7RWaHoSCP2tHbyUsK.do --password c=WHIVE 
 pause
